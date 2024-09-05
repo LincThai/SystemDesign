ee60ed3c3d5b418ecf4c9b6f2189664d486314f7
LincThai <78945845+LincThai@users.noreply.github.com>
2024-09-05 12:04:15 +1000

    Begin Prototyping first scriptable objects

---
+++ Assets/Scripts/Items.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SystemDesign.CraftingSystem
+{
+    public class Items : ScriptableObject
+    {
+        
+    }
+}
