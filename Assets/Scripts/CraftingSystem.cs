300baf16540f6a000c023e54f69f06c462d5da45
LincThai <78945845+LincThai@users.noreply.github.com>
2024-09-09 11:33:45 +1000

    Update CraftingSystem.cs

--- Assets/Scripts/CraftingSystem.cs
+++ Assets/Scripts/CraftingSystem.cs
@@ -2,17 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CraftingSystem : MonoBehaviour
+namespace SystemDesign.CraftingSystem
 {
-    // Start is called before the first frame update
-    void Start()
+    public class CraftingSystem : MonoBehaviour
     {
+        // set variables
+        // UI variables
+        [Header("UI Variables")]
+        // Array for crafting slots (UI)
+        public CraftingSlot[] craftingSlots;
+        // results slot
+        public CraftingSlot resultsSlot;
         
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        // list of given ingredients in the slots
+        public List<Items> ingredients;
+        // list of results (Potions). This also contains the recipe in the scriptable object
+        public List<Items> potions;
     }
 }
