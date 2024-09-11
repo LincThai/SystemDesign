7751386f761403a46768b452e28676cb89396154
LincThai <78945845+LincThai@users.noreply.github.com>
2024-09-11 10:44:20 +1000

    Completing UI Elements

---
+++ Assets/Scripts/IngredientSlot.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace SystemDesign.CraftingSystem
+{
+    public class IngredientSlot : MonoBehaviour
+    {
+        // set variable to store
+        public Items ingredient;
+        public Image thumbnail;
+    }
+}
