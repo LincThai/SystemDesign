15f421e452ce91ba975b77415c8ef725c23095c3
LincThai <78945845+LincThai@users.noreply.github.com>
2024-09-14 14:26:14 +1000

    more work

---
+++ Assets/Scripts/ResultsSlot.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using SystemDesign.CraftingSystem;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace SystemDesign.CraftingSyatem
+{
+    public class ResultsSlot : MonoBehaviour
+    {
+        // set the result Item variable
+        public Item item;
+        // reference the UI image thumbnail
+        public Image thumbnail;
+    }
+}
